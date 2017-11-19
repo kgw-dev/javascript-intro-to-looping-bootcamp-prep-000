fae81f1cb40b9bcc1a7b17df2218c7f8610cb428
kent winter <kgw700@gmail.com>
2017-11-19 02:20:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,3 +9,23 @@ function forLoop (array) {
 }
   return array
 }
+
+
+function whileLoop (number){
+    while (number > 0) {
+      console.log(--number)
+      return ("done")
+}
+
+function doWhileLoop (array) {
+  for (let i = 0; i < 25; i++) {
+  if (i===1){
+    array.push("I am 1 strange loop")
+  }
+  else {
+    array.push("I am $(i) strange loops")
+  }
+}
+  return array
+}
+
