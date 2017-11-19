664ccf0d775b490c92e58d483be1c890fbfe0328
kent winter <kgw700@gmail.com>
2017-11-19 02:23:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,15 +1,14 @@
 function forLoop (array) {
   for (let i = 0; i < 25; i++) {
-  if (i===1){
-    array.push("I am 1 strange loop")
+    if (i===1){
+      array.push("I am 1 strange loop")
+    }
+    else {
+      array.push("I am $(i) strange loops")
+    }
   }
-  else {
-    array.push("I am $(i) strange loops")
-  }
+return array
 }
-  return array
-}
-
 
 function whileLoop (number){
     while (number > 0) {
@@ -18,13 +17,4 @@ function whileLoop (number){
 }
 
 function doWhileLoop (array) {
-  for (let i = 0; i < 25; i++) {
-  if (i===1){
-    array.push("I am 1 strange loop")
-  }
-  else {
-    array.push("I am $(i) strange loops")
-  }
-}
-  return array
 }
